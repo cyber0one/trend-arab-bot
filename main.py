dedd07dea9ba1335ba715400c016860e99580055
cyber0one <42318932+cyber0one@users.noreply.github.com>
2025-11-01 20:06:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start(update, context):
     await update.message.reply_text("بوت الترند العربي جاهز ✅")
 
+async def on_startup(app):
+    await app.bot.delete_webhook(drop_pending_updates=True)
+
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    app.run_polling(on_startup=on_startup)
